057123b256d05a7a25b96b94c17d8f2cb5805f7e
leandro <leandro.moquiuti@gmail.com>
2022-04-08 11:09:52 -0400

    create entity's

---
+++ src/main/java/one/digitalinnovation/personalapi/entity/Phone.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.personalapi.entity;
+
+import lombok.*;
+import one.digitalinnovation.personalapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+}
---
+++ src/main/java/one/digitalinnovation/personalapi/enums/PhoneType.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.personalapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+}
